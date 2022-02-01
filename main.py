699e517812043636aec7fdf50cefda9d3432a890
Zhilet <pety.2003@yandex.ru>
2022-02-01 18:39:17 +0300

    score

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ FPS = 60
 pygame.init()
 screen = pygame.display.set_mode((1920, 1000))
 clock = pygame.time.Clock()
+font = pygame.font.Font(None, 40)
 
+
+# colors
+GRAY = (155, 155, 155)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -15,7 +19,11 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 PUMPKIN_COLOR = (255, 117, 24)
+
+# other things
+score = 0
 PLATFORM_SIZE_XY = (100, 30)
+right_panel_width = 200
 
 class Platform:
     def __init__(self, pos: tuple[2], size: tuple[2]) -> None:
@@ -32,8 +40,8 @@ class Platform:
         x += 7 * self.dir
         if x <= 0:
             x = 0
-        elif x + self.size[0] > screen.get_width():
-            x = screen.get_width() - self.size[0]
+        elif x + self.size[0] > screen.get_width() - right_panel_width:
+            x = screen.get_width() - self.size[0] - right_panel_width
 
         self.pos = (x, y)
         self.draw()
@@ -80,8 +88,8 @@ class Square:
         if x <= 0:
             x = 0
             vx = -vx
-        elif x + self.size > screen.get_width():
-            x = screen.get_width() - self.size
+        elif x + self.size > screen.get_width() - right_panel_width:
+            x = screen.get_width() - self.size - right_panel_width
             vx = -vx
 
         if y <= 0:
@@ -99,7 +107,9 @@ class Square:
 
 
 def draw_background():
-    pygame.draw.rect(screen, BLACK, (0, 0, screen.get_width(), screen.get_height()))
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, GRAY, (screen.get_width() - right_panel_width, 0, screen.get_width(), screen.get_height()))
+
 
 objects = set()
 for i in range(30):
@@ -130,7 +140,10 @@ while True:
     for obj in set(objects):
         obj.move(objects)
         platform.collision(obj)
-
-        
+    score += len(objects)
+    text1 = font.render(f'Очки: {(score)//100}', True, (255, 0, 0))
+    text2 = font.render(f'Живых: {(len(objects))}', True, (255, 0, 0))
+    screen.blit(text1, (screen.get_width() - right_panel_width, 0))
+    screen.blit(text2, (screen.get_width() - right_panel_width, 100))
     platform.move()
     pygame.display.update()
